8c330dd3d44ea52ccebf7fc827face052f22e2d4
PiusKariuki <piuskariuki19@gmail.com>
2023-08-09 10:56:05 +0300

    second

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+  <p class="text-blue-500">Hey buddy</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
